d32abc376a5b4b16f8788c1afb47c277d4161716
Sakib Hasan <sakib100.sa@gmail.com>
2023-10-20 19:00:54 +0600

    hero section complete

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,25 +1,70 @@
 import Image from "next/image";
+import Link from "next/link";
+import {
+    AiOutlineLinkedin,
+    AiOutlineHtml5,
+    AiOutlineDatabase,
+} from "react-icons/ai";
+import { BiLogoTelegram, BiLogoPython } from "react-icons/bi";
+import { FiGithub } from "react-icons/fi";
+import { TbBrandCss3, TbSql } from "react-icons/tb";
+import { RiJavascriptLine } from "react-icons/ri";
+
+
 const Hero = () => {
     return (
         <section className="mt-[5rem]">
-            <div className="w-full grid grid-cols-1 md:grid-cols-12">
+            <div className="w-full grid grid-cols-1 md:grid-cols-12 gap-5 items-center ">
                 <div className="col-span-1 md:col-span-8">
-                    <div className="title">
-                        <h1 className="text-[2rem]">
+                    <div className="flex flex-col gap-5 items-start">
+                        <h1 className="text-[1.8rem]">
                             <span className="bg-light-secondary-bg dark:bg-dark-secondary px-2 py-1">
                                 MERN - Stack
                             </span>{" "}
                             <br /> Developer
                         </h1>
+                        <p className="text-[1.3rem]">
+                            Hi,I'm Mamdud Hasan, a detail-oriented backend
+                            software engineer dedicated to crafting robust and
+                            efficient systems that power seamless user
+                            experiences.
+                        </p>
+                        <div className="flex flex-row  items-center gap-5">
+                            <Link
+                                href="https://github.com/n8fury"
+                                target="_blank"
+                            >
+                                <FiGithub className="text-[1.8rem] cursor-pointer" />
+                            </Link>
+                            <Link href="https://t.me/n8fury" target="_blank">
+                                <BiLogoTelegram className="text-[1.8rem] cursor-pointer" />
+                            </Link>
+                            <Link
+                                href="https://www.linkedin.com/in/mamdud-hasan/"
+                                target="_blank"
+                            >
+                                <AiOutlineLinkedin className="text-[1.8rem] cursor-pointer" />
+                            </Link>
+                        </div>
                     </div>
                 </div>
-                <div className="col-span-1 md:col-span-4 bg-light-secondary-bg dark:bg-dark-secondary rounded-[2xl] relative ">
+                <div className="col-span-1 md:col-span-4 flex justify-center ">
                     <Image
                         src="/images/profile-picture.png"
                         width={300}
                         height={450}
-                        className="absolute left-[50%] translate-x-[-50%]"
+                        className="bg-light-secondary-bg dark:bg-dark-secondary rounded-[2rem]"
                     />
+                </div>
+            </div>
+            <div className="w-full flex flex-col md:flex-row justify-center items-center gap-2  md:gap-5 my-[5rem]">
+                <h3 className="text-[2rem] font-[600]">Tech - Stack</h3>
+                <div>
+                    <div className="hidden md:inline-block border-[1.5px] border-primary-text dark:border-dark-text h-10" />
+                </div>
+                <div className="flex flex-row flex-wrap gap-5 md:gap-10 text-[2.5rem]">
+                    <AiOutlineHtml5 /> <TbBrandCss3 /> <RiJavascriptLine />
+                    <BiLogoPython /> <AiOutlineDatabase /> <TbSql />
                 </div>
             </div>
         </section>
